54bb64a058f1308329ba6df4e64605ad88d0b33a
Cade <39470462+cadedopp@users.noreply.github.com>
2018-08-21 17:46:48 -0600

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,8 +18,8 @@ export class Contact extends React.Component{
     return (
       <article id="contact" className={contactClass}>
         <h2 className="major">Contact</h2>
-        <p>If you have questions or want to discuss possible solutions for your students, you can contact Cade Dopp at: <br /></p>
-        <a href="mailto:educell@cadedopp.com" target="_blank"> educell@cadedopp.com</a>
+        <p>If you have questions or want to discuss possible solutions for your students, you can contact us at: <br /></p>
+        <a href="mailto:info@educell.org" target="_blank"> info@educell.org</a>
         <div onClick={() => this.props.endTransitionHandler('contact')} className="close">Close</div>
       </article>
     );
