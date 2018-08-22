0081f68607f6f1af84e488b26609bde454738267
Cade <39470462+cadedopp@users.noreply.github.com>
2018-08-21 18:44:16 -0600

    Update Donate.jsx

--- src/components/Donate.jsx
+++ src/components/Donate.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
+import 501 from '../images/501c3approved.pdf';
+import articles from '../images/educellarticles.pdf';
 
 export class Donate extends React.Component{
 
@@ -20,8 +22,8 @@ export class Donate extends React.Component{
         <h2 className="major">Donate</h2>
         <span className="image main"><img src="images/pic01.jpg" alt="" /></span>
         <p>We are waiting to get permission from the state of Utah before we can accept donations.</p>
-        <p> <a href="../images/501c3approved.pdf" target="_blank">501(c)(3) determination letter</a></p>
-        <p> <a href="../images/educellarticles.pdf" target="_blank">Articles of incorporation</a></p>
+        <p> <a href={501} target="_blank">501(c)(3) determination letter</a></p>
+        <p> <a href={articles} target="_blank">Articles of incorporation</a></p>
         <div onClick={() => this.props.endTransitionHandler('donate')} className="close">Close</div>
       </article>
     );
