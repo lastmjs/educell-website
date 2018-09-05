f582c03e0ab49c4a9eb859e03e6d84529a140090
Cade <39470462+cadedopp@users.noreply.github.com>
2018-09-05 17:53:03 -0600

    Update Resources.jsx

--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -26,8 +26,9 @@ export class Resources extends React.Component{
         <h2 className="major">Resources</h2>
    
     <div class="image_text_column">
-
-    <h4>Resource Category 1</h4>
+      <h3> Technology </h3>
+      
+    <h4>Network</h4>
 
       <div class="image_text_row">
         <div class="image_row">
@@ -35,10 +36,11 @@ export class Resources extends React.Component{
         </div>
 
         <p class="text_row">
+          Setup a network.
         </p>
       </div>
 
-    <h4>Resource Category 2</h4>
+    <h4>Devices</h4>
 
       <div class="image_text_row">
         <div class="image_row">
@@ -46,9 +48,62 @@ export class Resources extends React.Component{
         </div>
 
         <p class="text_row">
+          Select devices.
         </p>
       </div>
     </div>
+        
+            <h4>Software</h4>
+
+      <div class="image_text_row">
+        <div class="image_row">
+        <img src={resource3img} width="100%" />*
+        </div>
+
+        <p class="text_row">
+          Select software.
+        </p>
+      </div>
+        
+         <h3> Curriculum </h3>
+      
+    <h4>Math Assessments</h4>
+
+      <div class="image_text_row">
+        <div class="image_row">
+        <img src={resource4img} width="100%" />*
+        </div>
+
+        <p class="text_row">
+          Assessments for students.
+        </p>
+      </div>
+
+    <h4>Math Competency Frameworks</h4>
+
+      <div class="image_text_row">
+        <div class="image_row">
+        <img src={resource5img} width="100%" />
+        </div>
+
+        <p class="text_row">
+          Select devices.
+        </p>
+      </div>
+    </div>
+        
+            <h4>Math Teaching Materials</h4>
+
+      <div class="image_text_row">
+        <div class="image_row">
+        <img src={resource6img} width="100%" />*
+        </div>
+
+        <p class="text_row">
+          Select software.
+        </p>
+      </div>
+      </div>
         
         <div onClick={() => this.props.endTransitionHandler('resources')} className="close">Close</div>
       </article>
