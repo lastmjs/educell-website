ed6b295c0c243255c722c87034bc07f18cdde22c
Cade <39470462+cadedopp@users.noreply.github.com>
2018-09-05 10:45:13 -0600

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -21,8 +21,8 @@ export class Contact extends React.Component{
         <p>We would love to provide you with a free consultation session. We can recommend free resources, offer suggestions, and explore opportunities for more robust (probably not free) solutions.
           If you want to discuss solutions for your students, or you have any other questions, you can contact us by:</p>
         <p>Email: <a href="mailto:info@educell.org" target="_blank"> info@educell.org</a></p>
-        <p>Phone: 801-682-3795 (within the United States)</p>
-        <p>WhatsApp: +1 801-682-3795</p>
+        <p>Phone: <a href="tel:+1801-682-3795">801-682-3795</a> (within the United States)</p>
+        <p>WhatsApp: <a href="htpps://wa.me/18016823795>+1 801-682-3795</a></p>
           
         <div onClick={() => this.props.endTransitionHandler('contact')} className="close">Close</div>
       </article>
