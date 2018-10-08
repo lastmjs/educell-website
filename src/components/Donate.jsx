6c6f3d53c25f2be5a67369d6429d696ac4f575fa
Cade <39470462+cadedopp@users.noreply.github.com>
2018-10-08 08:49:01 +0000

    Update Donate.jsx

--- src/components/Donate.jsx
+++ src/components/Donate.jsx
@@ -19,7 +19,7 @@ export class Donate extends React.Component{
       <article id="donate" className={donateClass}>
         <h2 className="major">Donate</h2>
         <span className="image main"><img src="images/pic01.jpg" alt="" /></span>
-        <p><a href='https://onetoday.google.com/p/hebnurli?c=6272147914358784'>Donate</a> to our current project on Google One Today.</p>
+        <p><a href='https://onetoday.google.com/p/hebnurli?c=6272147914358784' target='_blank'>Donate</a> to our current project on Google One Today.</p>
         <p>Please contact us at <a href="mailto:info@educell.org" target="_blank"> info@educell.org</a> to learn about other ways you can make a difference.</p>
         <p> <a href='../501-c-3-approved.pdf' target="_blank">501(c)(3) determination letter</a></p>
         <p> <a href='../educell-articles.pdf' target="_blank">Articles of incorporation</a></p>
